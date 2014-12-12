67170d08903046720bab7bf9334d979b75d0271b
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-11 21:52:40 -0500

    Add Item class

---
+++ RandomItems/RandomItems/Item.h
@@ -0,0 +1,13 @@
+//
+//  Item.h
+//  RandomItems
+//
+//  Created by Ricky Pattillo on 12/11/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+
+@end
---
+++ RandomItems/RandomItems/Item.m
@@ -0,0 +1,13 @@
+//
+//  Item.m
+//  RandomItems
+//
+//  Created by Ricky Pattillo on 12/11/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@end

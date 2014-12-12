4deedae59efe7582fecc7deef95fc5ff893dce5d
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-11 21:53:25 -0500

    Add accessors for Item class

--- RandomItems/RandomItems/Item.h
+++ RandomItems/RandomItems/Item.h
@@ -9,5 +9,22 @@
 #import <Foundation/Foundation.h>
 
 @interface Item : NSObject
+{
+   NSString *_itemName;
+   NSString *_serialNumber;
+   int _valueInDollars;
+   NSDate *_dateCreated;
+}
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
 
 @end
--- RandomItems/RandomItems/Item.m
+++ RandomItems/RandomItems/Item.m
@@ -10,4 +10,45 @@
 
 @implementation Item
 
+- (void)setItemName:(NSString *)str
+{
+   _itemName = str;
+}
+
+
+- (NSString *)itemName
+{
+   return _itemName;
+}
+
+
+- (void)setSerialNumber:(NSString *)str
+{
+   _serialNumber = str;
+}
+
+
+- (NSString *)serialNumber
+{
+   return _serialNumber;
+}
+
+
+- (void)setValueInDollars:(int)v
+{
+   _valueInDollars = v;
+}
+
+
+- (int)valueInDollars
+{
+   return _valueInDollars;
+}
+
+
+- (NSDate *)dateCreated
+{
+   return _dateCreated;
+}
+
 @end

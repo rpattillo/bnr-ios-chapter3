7d01059f60dbdc6e9e95e5ccfa2a244dc525133d
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-13 19:00:26 -0500

    Replace manual ivars and accessors with properties
    This is not precisely identical yet because the property attributes
    are not yet declared and the defaults are different than what manual
    ivars and accessors produce.
    
    Additionally the setter for _containedItem does not have the extra
    functionality.

--- RandomItems/RandomItems/Item.h
+++ RandomItems/RandomItems/Item.h
@@ -9,15 +9,15 @@
 #import <Foundation/Foundation.h>
 
 @interface Item : NSObject
-{
-   NSString *_itemName;
-   NSString *_serialNumber;
-   int _valueInDollars;
-   NSDate *_dateCreated;
-   
-   Item *_containedItem;
-   __weak Item *_container;
-}
+
+@property NSString *itemName;
+@property NSString *serialNumber;
+@property int valueInDollars;
+@property NSDate *dateCreated;
+
+@property Item *containedItem;
+@property Item *container;
+
 
 + (instancetype)randomItem;
 
@@ -27,23 +27,5 @@
                     serialNumber:(NSString *)sNumber;
 
 - (instancetype)initWithItemName:(NSString *)name;
-
-
-- (void)setItemName:(NSString *)str;
-- (NSString *)itemName;
-
-- (void)setSerialNumber:(NSString *)str;
-- (NSString *)serialNumber;
-
-- (void)setValueInDollars:(int)v;
-- (int)valueInDollars;
-
-- (NSDate *)dateCreated;
-
-- (void)setContainedItem:(Item *)item;
-- (Item *)containedItem;
-
-- (void)setContainer:(Item *)item;
-- (Item *)container;
 
 @end
